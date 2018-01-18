8568eb4b12c2e4fd0651e171d54978235efbad71
Scott Haley <williamscotthaley@gmail.com>
2018-01-17 20:38:47 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app" class="card">
+    <top-bar></top-bar>
+  </div>
+</template>
+
+<script>
+import TopBar from './components/TopBar'
+
+export default {
+  name: 'App',
+  components: {TopBar}
+}
+</script>
+
+<style>
+body {
+  font-family: 'Courier New', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: #EBEBEB;
+}
+
+#app {
+  display: inline-block;
+  margin: 30 auto;
+  background-color: white;
+  width: 90%;
+}
+
+.card {
+  -webkit-box-shadow: 0 16px 24px 2px rgba(0,0,0,0.14), 0 6px 30px 5px rgba(0,0,0,0.12), 0 8px 10px -5px rgba(0,0,0,0.3);
+  box-shadow: 0 16px 24px 2px rgba(0,0,0,0.14), 0 6px 30px 5px rgba(0,0,0,0.12), 0 8px 10px -5px rgba(0,0,0,0.3);
+}
+
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="topbar">
+    <div class="title">
+      <h1>Influence</h1>
+    </div>
+    <div class="profile">
+      <div class="profile__info">
+        <span>@scotthaley</span>
+        <span>12,000</span>
+        <a href='#'>logout</a>
+      </div>
+      <img src="http://www.evatar.io/4f42226f69844d535deb9b9d2763eee73e03f9cfc9505d565cf2e9275c14c116"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar'
+}
+</script>
+
+<style lang="scss">
+.topbar {
+  display: flex;
+  flex-direction: row;
+  height: 100px;
+  text-align: left;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+h1 {
+  font-weight: 400;
+  font-size: 2.5em;
+}
+
+a {
+  text-decoration: none;
+}
+
+.title {
+  flex-grow: 1;
+}
+
+.profile {
+  display: flex;
+  flex-direction: row;
+
+  &__info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+    padding-right: 20px;
+  }
+
+  img {
+    width: 80px;
+    height: 80px;
+    align-self: center;
+  }
+}
+</style>
